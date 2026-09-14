f917936fc634462a66660ad22c9ea98788dda7c9
paipai <paipai@paipaideMacBook-Air.local>
2026-09-14 19:27:28 +0800

    更新整理书架为15帧

--- demo/gen_demo.py
+++ demo/gen_demo.py
@@ -17,7 +17,7 @@ data = {
     "mastered": load_frames("mastered", 16),
     "forgotten": load_frames("forgotten", 16),
     "newnote": load_frames("newnote", 16),
-    "newbook": load_frames("newbook", 16),
+    "newbook": load_frames("newbook", 15),
 }
 
 frames_json = json.dumps(data)
